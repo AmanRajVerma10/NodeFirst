83e898d6b02fe984adaa2e6134dc2f30040a864f
AmanRajVerma10 <verma.aman464@gmail.com>
2024-02-02 18:39:41 +0530

    working with req and res

--- app.js
+++ app.js
@@ -1,7 +1,32 @@
 const http = require("http");
 
 let server = http.createServer((req, res) => {
-  console.log("aman");
+  let url=req.url;
+  if(url==='/home'){
+    res.setHeader('Content-Type','text/html');
+    res.write('<html>');
+    res.write('<head><title>My Home Page</title></head>');
+    res.write('<body><h1>Welcome Home</h1></body>')
+    res.write('</html>');
+    return res.end();
+
+  }
+  if(url==='/node'){
+    res.setHeader('Content-Type','text/html');
+    res.write('<html>');
+    res.write('<head><title>My Node Page</title></head>');
+    res.write('<body><h1>Welcome to my nodeJs project</h1></body>')
+    res.write('</html>');
+    return res.end();
+
+
+  }
+  res.setHeader('Content-Type','text/html');
+  res.write('<html>');
+  res.write('<head><title>My first Page</title></head>');
+  res.write('<body><h1>Hello from Node Server</h1></body>')
+  res.write('</html>');
+  res.end();
 });
 
 server.listen(4000);
