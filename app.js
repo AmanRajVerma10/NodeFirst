cf6c6c7a66c6b99f296cf4a08de08a0a12ee1b47
AmanRajVerma10 <verma.aman464@gmail.com>
2024-02-05 15:54:21 +0530

    ExpressInstalledAndUsed

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 const http = require("http");
-const routes= require('./routes')
 
-let server = http.createServer(routes);
+const express = require("express");
 
-server.listen(4000);
+const app = express();
+
+app.use((req, res, next) => {
+  console.log("in mw1");
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log("in mw2");
+  res.send('<h1>Welcome</h1>')
+});
+
+app.listen(3000);
