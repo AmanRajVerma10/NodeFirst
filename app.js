2568bed1087ce73d71c13a76320ec06c9a098249
AmanRajVerma10 <verma.aman464@gmail.com>
2024-02-05 18:22:48 +0530

    clean up

--- app.js
+++ app.js
@@ -2,22 +2,20 @@ const express = require("express");
 const bodyParser= require("body-parser");
 
 const app = express();
+
+const adminRoutes= require('./routes/admin')
+const shopRoutes= require('./routes/shop')
+
 app.use(bodyParser.urlencoded({extended:false}))
 
-app.use("/add-product", (req, res, next) => {
-  res.send(
-    '<form action="/product" method="POST"><input type="text" name="title"></input><button type="submit">Add</button></form>'
-  );
-});
+app.use(adminRoutes);
+app.use(shopRoutes);
 
-app.use('/product',(req,res,next)=>{
-    console.log(req.body);
-    res.redirect('/');
+app.use((req,res,next)=>{
+    res.status(404).send('<h1>Page Not Found</h1>')
 })
 
-app.use("/", (req, res, next) => {
-  console.log("in mw2");
-  res.send("<h1>Welcome</h1>");
-});
+
+
 
 app.listen(3000);
