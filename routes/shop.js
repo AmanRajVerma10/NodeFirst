1e8c291e0d4260b847e8ce6d6f632992cee0c277
AmanRajVerma10 <verma.aman464@gmail.com>
2024-02-07 16:30:09 +0530

    helper function used

--- routes/shop.js
+++ routes/shop.js
@@ -1,29 +1,36 @@
-const express= require('express');
-const fs= require('fs');
+const express = require("express");
+const path= require('path')
 
-const router= express.Router();
+const rootDir= require('../util/path')
 
-router.get("/", (req, res, next) => {
-  fs.readFile('messages.txt',(err,data)=>{
-    if(err){
-      data="No Messages Found"
-    }
-    res.send(`${data}<form action="/" method="POST" onsubmit="document.getElementById('username').value=localStorage.getItem('username')">
-    <input id="chat" type="text" name="msg" placeholder="Message"></input>
-    <input id="username" type="hidden" name="usr"></input>
-    <button type="submit">Send</button></form>`)
+const router = express.Router();
 
-  })
- 
-  });
+router.get("/",(req,res,next)=>{
+  res.sendFile(path.join(rootDir,'views','shop.html'));
+})
 
-  router.post("/",(req,res,next)=>{
-    console.log(req.body);
-    fs.writeFile('messages.txt',`${req.body.usr}:${req.body.msg}`,{flag:'a'},(err)=>{
-      console.log(err);
-      res.redirect('/');
-    })
-    
-  })
+// router.get("/", (req, res, next) => {
+//   fs.readFile("messages.txt", (err, data) => {
+//     if (err) {
+//       data = "No Messages Found";
+//     }
+//     res.send(`${data}<form action="/" method="POST" onsubmit="document.getElementById('username').value=localStorage.getItem('username')">
+//     <input id="chat" type="text" name="msg" placeholder="Message"></input>
+//     <input id="username" type="hidden" name="usr"></input>
+//     <button type="submit">Send</button></form>`);
+//   });
+// });
 
-  module.exports= router
+// router.post("/", (req, res, next) => {
+//   console.log(req.body);
+//   fs.writeFile(
+//     "messages.txt",
+//     `${req.body.usr}:${req.body.msg}`,
+//     { flag: "a" },
+//     (err) => {
+//       err ? console.log(err) : res.redirect("/");
+//     }
+//   );
+// });
+
+module.exports = router;
