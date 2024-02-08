f088b1f18ba40e56e52a2856bdfce03200cd938b
AmanRajVerma10 <verma.aman464@gmail.com>
2024-02-09 02:51:09 +0530

    Contact page added

---
+++ routes/contact.js
@@ -0,0 +1,16 @@
+const express= require('express');
+const path=require('path');
+const rootDir= require('../util/path')
+
+const router= express.Router();
+
+router.get('/contact',(req,res,next)=>{
+    res.sendFile(path.join(rootDir,'views','contactus.html'))
+
+})
+
+router.post('/success',(req,res,next)=>{
+    res.send('<h1>Form Successfully Filled</h1>')
+})
+
+module.exports=router;
